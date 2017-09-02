836fca52bc159406b9e55f93cbd04a14ccc090dd
Henry Lee <henry@henryiMac.local>
2017-09-03 02:22:23 +0800

    update

--- 7.PHP/test.php
+++ 7.PHP/test.php
@@ -2,9 +2,9 @@
 // isset 对null过滤，而empty对所有false值过滤
 header("content-type:text/html;charset=utf-8");
 if (isset($_POST['ac']) && $_POST['ac'] == 'login') {
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	echo 'User:' . $username . ', Password:'.$password.'<hr>';
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    echo 'User:' . $username . ', Password:'.$password.'<hr>';
 }
 ?>
 <form method="post" accept-charset="utf-8">
