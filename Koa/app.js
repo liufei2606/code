d308f0b9639fbfb3cbc8145c2e685ea590b2f44e
Henry <liboming88@yeah.net>
2017-11-10 21:38:24 +0800

    add koa await

--- Koa/app.js
+++ Koa/app.js
@@ -4,13 +4,49 @@ const Koa = require('koa');
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
+app.use(async(ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+});
+
+app.use(async(ctx, next) => {
+    const start = new Date().getTime(); // 当前时间
+    await next(); // 调用下一个middleware
+    const ms = new Date().getTime() - start; // 耗费时间
+    console.log(`Time: ${ms}ms`); // 打印耗费时间
+});
+
 // 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
 
+// app.use(async(ctx, next) => {
+//     if (ctx.request.path === '/') {
+//         ctx.response.body = 'index page';
+//     } else {
+//         await next();
+//     }
+// });
+
+// app.use(async(ctx, next) => {
+//     if (ctx.request.path === '/test') {
+//         ctx.response.body = 'TEST page';
+//     } else {
+//         await next();
+//     }
+// });
+
+// app.use(async(ctx, next) => {
+//     if (ctx.request.path === '/error') {
+//         ctx.response.body = 'ERROR page';
+//     } else {
+//         await next();
+//     }
+// });
+
 // 在端口3000监听:
-app.listen(3000);
-console.log('app started at port 3000...');
+app.listen(3030);
+console.log('app started at port 3030...');
