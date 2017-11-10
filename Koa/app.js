280652622dd578b5baffdc24bb4ce65c898074ff
Henry <liboming88@yeah.net>
2017-11-10 21:48:36 +0800

    add koa-router

--- Koa/app.js
+++ Koa/app.js
@@ -1,27 +1,28 @@
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
+const router = require('koa-router')();
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
-app.use(async(ctx, next) => {
-    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
-    await next(); // 调用下一个middleware
-});
+// app.use(async(ctx, next) => {
+//     console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+//     await next(); // 调用下一个middleware
+// });
 
-app.use(async(ctx, next) => {
-    const start = new Date().getTime(); // 当前时间
-    await next(); // 调用下一个middleware
-    const ms = new Date().getTime() - start; // 耗费时间
-    console.log(`Time: ${ms}ms`); // 打印耗费时间
-});
+// app.use(async(ctx, next) => {
+//     const start = new Date().getTime(); // 当前时间
+//     await next(); // 调用下一个middleware
+//     const ms = new Date().getTime() - start; // 耗费时间
+//     console.log(`Time: ${ms}ms`); // 打印耗费时间
+// });
 
-// 对于任何请求，app将调用该异步函数处理请求：
-app.use(async(ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
+// // 对于任何请求，app将调用该异步函数处理请求：
+// app.use(async(ctx, next) => {
+//     await next();
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = '<h1>Hello, koa2!</h1>';
+// });
 
 // app.use(async(ctx, next) => {
 //     if (ctx.request.path === '/') {
@@ -47,6 +48,24 @@ app.use(async(ctx, next) => {
 //     }
 // });
 
+// log request URL:
+app.use(async(ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// add url-route:
+router.get('/hello/:name', async(ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async(ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes());
 // 在端口3000监听:
 app.listen(3030);
 console.log('app started at port 3030...');
