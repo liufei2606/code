c237e5e5da970a9ae59da5539e49c1cf7de813db
Henry <liboming88@yeah.net>
2017-11-11 08:23:17 +0800

    add koa nunjucks

--- Koa/app.js
+++ Koa/app.js
@@ -1,15 +1,53 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
+const nunjucks = require('nunjucks');
 
 const app = new Koa();
+
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function(n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
 
 // log request URL:
 app.use(async(ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
 });
-
+app.use(async(ctx, next) => {
+    if (ctx.request.path === '/template') {
+        var s = env.render('hello.html', { name: '<script>alert("小明")</script>' });
+        console.log(s);
+        await next();
+    }
+});
 // parse request body:
 app.use(bodyParser());
 
