3052bfd8d0ece19470f7d13b29d5ce7094e059b9
Henry <liboming88@yeah.net>
2017-11-12 21:24:42 +0800

    add rest mvc

--- REST/app.js
+++ REST/app.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
+const templating = require('./templating');
+const rest = require('./rest');
 
 const app = new Koa();
 
@@ -10,10 +12,23 @@ app.use(async(ctx, next) => {
     await next();
 });
 
+// static file support:
+let staticFiles = require('./static-files');
+app.use(staticFiles('/static/', __dirname + '/static'));
+
 // parse request body:
 app.use(bodyParser());
 
-// add controller:
+// add nunjucks as view:
+app.use(templating('views', {
+    noCache: true,
+    watch: true
+}));
+
+// bind .rest() for ctx:
+app.use(rest.restify());
+
+// add controllers:
 app.use(controller());
 
 app.listen(3000);
