c015e6dcae16a38f3547e39db334a6993f697ee0
Henry <liboming88@yeah.net>
2017-11-17 11:39:14 +0800

    add composer twig

--- PHP/test.php
+++ PHP/test.php
@@ -1,15 +1,24 @@
 <?php
 // isset 对null过滤，而empty对所有false值过滤
-header("content-type:text/html;charset=utf-8");
-if (isset($_POST['ac']) && $_POST['ac'] == 'login') {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    echo 'User:' . $username . ', Password:'.$password.'<hr>';
+// header("content-type:text/html;charset=utf-8");
+// if (isset($_POST['ac']) && $_POST['ac'] == 'login') {
+//     $username = $_POST["username"];
+//     $password = $_POST["password"];
+//     echo 'User:' . $username . ', Password:'.$password.'<hr>';
+// }
+
+
+
+$arr = [1,3,5,6];
+foreach ($arr as &$v) {
 }
-?>
-<form method="post" accept-charset="utf-8">
-User:<input type="text" name="username" value="" placeholder="">
-Password:<input type="text" name="password" value="" placeholder="">
-<input type="submit" value="submit">
-<input type="hidden" name="ac" value="login">
-</form>
+foreach ($arr as $v) {
+}
+var_dump($arr);
+$array = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+var_dump($array);
