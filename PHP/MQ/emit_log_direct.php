ad7962bc59107222589d3e115045b605ffaf990d
Henry <liboming88@yeah.net>
2018-04-01 14:21:25 +0800

    add queue route

---
+++ PHP/MQ/emit_log_direct.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->exchange_declare('direct_logs', 'direct', false, false, false);
+$serverity = isset($argv[1]) && !empty($argv[1]) ? $argv[1] : 'info';
+
+$data = implode(' ', array_slice($argv, 2));
+
+if (empty($data)) {
+    $data = "info: Hello World!";
+}
+$msg = new AMQPMessage($data);
+
+$channel->basic_publish($msg, 'direct_logs', $serverity);
+
+echo " [x] Sent ",$serverity, ':', $data, "\n";
+
+$channel->close();
+$connection->close();
+// php PHP/MQ/emit_log_direct.php warning "Run"
