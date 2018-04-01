8797efbbaed4827e8b0afc18c0e610d95fccff91
Henry <liboming88@yeah.net>
2018-04-01 20:03:17 +0800

    add mq bind topic

---
+++ PHP/MQ/emit_log_topic.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->exchange_declare('direct_logs', 'direct', false, false, false);
+$route_key = isset($agrv[1]) && !empty($argv[1]) ? $argv[1] : 'annonymous.info';
+
+$data = implode(' ', array_slice($argv, 2));
+
+if (empty($data)) {
+    $data = "Hello World!";
+}
+
+$msg = new AMQPMessage($data);
+
+$channel->basic_publish($msg, 'topic_logs', $route_key);
+
+echo " [x] Sent ",$route_key, ':', $data, "\n";
+
+$channel->close();
+$connection->close();
+
+// php emit_log_topic.php "kern.critical" "A critical kernel error"
