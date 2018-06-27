709b66793dde6ef6772b2bbf23ce18cd9ad30756
Henryli <liboming88@yeah.net>
2018-06-27 18:01:40 +0800

    add php oop object,todo namesapce

---
+++ PHP/Oop/Obj/BookProduct.php
@@ -0,0 +1,32 @@
+<?php
+namespace Demo1;
+
+// use Demo1\ShopProduct;
+require './ShopProduct.php';
+
+class BookProduct extends ShopProduct
+{
+    private $numPages = 0;
+
+    public function __construct($title, $firstName, $mainName, $price, $numPages)
+    {
+        parent::__construct($title, $firstName, $mainName, $price);
+        $this->numPages = $numPages;
+    }
+
+    public function getNumPages()
+    {
+        return $this->numPages;
+    }
+
+    public function getSummeryLine()
+    {
+        $base = parent::getSummeryLine();
+        return $base . ":page count - {$this->numPages}";
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
---
+++ PHP/Oop/Obj/ShopProduct.php
@@ -0,0 +1,61 @@
+<?php
+namespace Demo1;
+
+class ShopProduct
+{
+    private $title;
+    private $producerMainName;
+    private $producerFirstName;
+    protected $price;
+    private $discount = 0;
+
+    public function __construct($title, $firstName, $mainName, $price)
+    {
+        $this->title = $title;
+        $this->producerFirstName = $firstName;
+        $this->producerMainName = $mainName;
+        $this->price = $price;
+    }
+
+    public function getProducerFirstName()
+    {
+        return $this->producerFirstName;
+    }
+
+    public function getProducerMainName()
+    {
+        return $this->producerMainName;
+    }
+
+    public function setDiscount($num)
+    {
+        $this->discount = $num;
+    }
+
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getPrice()
+    {
+        return ($this->price - $this->discount);
+    }
+
+    public function getProducer()
+    {
+        return "{$this->producerFirstName}" . " {$this->producerMainName}";
+    }
+
+    public function getSummeryLine()
+    {
+        $base = "{$this->title} ( {$this->producerFirstName}, ";
+        $base .= "{$this->producerMainName} )";
+        return $base;
+    }
+}
