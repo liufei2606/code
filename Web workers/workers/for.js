2ed1033b701d153612e70db6d18719380d1e2d90
Henryli <liboming88@yeah.net>
2018-08-05 18:28:50 +0800

    add non blocking

--- Web workers/workers/for.js
+++ Web workers/workers/for.js
@@ -1,3 +1,9 @@
-setTimeout(() => {
-    postMessage('Hello World!');
-}, 5000);
+// for.js
+self.onmessage = function(event) {
+    var x = 0;
+    for (var i = 0; i < 200000000; i++) {
+        x = x + i;
+    }
+    self.postMessage(x);
+    self.close();
+}
