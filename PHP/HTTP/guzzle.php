8ce30d65d7345503e576f8bcd9d6576882d895f6
henry <liboming88@yeah.net>
2018-12-17 16:41:55 +0800

    test request

--- PHP/HTTP/guzzle.php
+++ PHP/HTTP/guzzle.php
@@ -2,13 +2,41 @@
 
 require '../vendor/autoload.php';
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 'on');
 
-$client = new GuzzleHttp\Client();
+// $client = new GuzzleHttp\Client();
 
-$res = $client->request('GET', 'https://api.github.com/user', [
-    'auth' => ['name', 'pass&']
+// $res = $client->request('GET', 'https://api.github.com/user', [
+//     'auth' => ['name', 'pass&']
+// ]);
+// echo $res->getStatusCode(); // "200"
+// echo $res->getHeader('content-type');  // 'application/json; charset=utf8'
+
+$client = new Client([
+    'base_uri' =>'http://httpbin.org',
+    'timeout' =>2.0
 ]);
-echo $res->getStatusCode(); // "200"
-echo $res->getHeader('content-type');  // 'application/json; charset=utf8'
+$response = $client->request('GET', 'get');
+echo $response->getStatusCode() . "<br>";
+
+$response = $client->delete('http://httpbin.org/delete');
+echo $response->getStatusCode() . '<br>';
+$response = $client->head('http://httpbin.org/get');
+echo $response->getStatusCode() . '<br>';
+$response = $client->options('http://httpbin.org/get');
+echo $response->getStatusCode() . '<br>';
+$response = $client->patch('http://httpbin.org/patch');
+echo $response->getStatusCode() . '<br>';
+$response = $client->post('http://httpbin.org/post');
+echo $response->getStatusCode() . '<br>';
+$response = $client->put('http://httpbin.org/put');
+echo $response->getStatusCode() . '<br>';
+
+
+$request = new Request('PUT', 'http://httpbin.org/put');
+$response = $client->send($request, ['timeout' => 2]);
+var_dump($response);
