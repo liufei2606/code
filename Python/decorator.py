9e8bf1ca8da13a028a6959d5d56062a6030301b8
Henry <liboming88@yeah.net>
2018-12-31 15:51:50 +0800

    add decorator

---
+++ Python/decorator.py
@@ -0,0 +1,11 @@
+def say_hello():
+  print("[DEBUG]: enter say_hello()")
+  print("hello!")
+
+def say_goodbye():
+   print("[DEBUG]: enter say_goodbye()")
+   print("Goodbye!")
+
+if __name__ == '__main__':
+   say_hello()
+   say_goodbye()
