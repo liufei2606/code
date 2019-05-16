80d2341ba7a7a17961ccec52ebd0b8e4715efba0
Henry <liboming88@yeah.net>
2019-05-16 20:25:03 +0800

    add namespace example

--- PHP/Synax/Oop/namespace/declare.php
+++ PHP/Synax/Oop/namespace/declare.php
@@ -4,10 +4,30 @@ namespace Bar\subnamespace;
 const FOO = 1;
 function foo()
 {
+	echo 'function';
 }
 class foo
 {
+	public function __construct() {
+		echo 'My class construct';
+	}
+
     public static function staticmethod()
     {
     }
 }
+
+new foo();
+echo '<br>';
+new \Bar\subnamespace\foo();
+echo '<br>';
+foo();
+echo '<br>';
+\Bar\subnamespace\foo();
+echo '<br>';
+namespace\foo();
+
+echo '<br>';
+echo namespace\FOO;
+echo '<br>';
+echo FOO;
