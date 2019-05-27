b13a7465609ff2afee957641cb440a9b7aabadbf
Henry <liboming88@yeah.net>
2019-05-27 20:21:23 +0800

    add item data and select

--- JavaScript/TypeScript/MySQL/src/index.ts
+++ JavaScript/TypeScript/MySQL/src/index.ts
@@ -1,20 +1,34 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {Catalog} from "./entity/Catalog";
 
 createConnection().then(async connection => {
 
     console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+	let catalog = new Catalog();
+	catalog.journal = "Oracle Magazine";
+	catalog.publisher = "Oracle Publishing";
+	catalog.edition = "March-April 2005";
+	catalog.title = "Starting with Oracle ADF";
+	catalog.author = "Steve Muench";
+	catalog.isPublished = true;;
+	await connection.manager.save(catalog);
 
+	console.log("Saved a new user with id: " + catalog.id);
+
+	let catalog2 = new Catalog();
+	catalog2.journal = "Oracle Magazine";
+	catalog2.publisher = "Oracle Publishing";
+	catalog2.edition = "November December 2013";
+	catalog2.title = "Engineering as a Service";
+	catalog2.author = "David A. Kelly";
+	catalog2.isPublished = true;
+
+	await connection.manager.save(catalog2);
+	console.log("Saved a new user with id: " + catalog2.id);
     console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    const Catalogs = await connection.manager.find(Catalog);
+    console.log("Loaded users: ", Catalogs);
 
     console.log("Here you can setup and run express/koa/any other framework.");
 
