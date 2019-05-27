42978c40f25b6ed44ab3941fd6ccb7d3f1a94c0b
Henry <liboming88@yeah.net>
2019-05-27 23:24:39 +0800

    add function add select update  delete

--- JavaScript/TypeScript/MySQL/src/index.ts
+++ JavaScript/TypeScript/MySQL/src/index.ts
@@ -3,6 +3,7 @@ import {createConnection} from "typeorm";
 import {Catalog} from "./entity/Catalog";
 
 createConnection().then(async connection => {
+	let catalogRepository = connection.getRepository(Catalog);
 
     console.log("Inserting a new user into the database...");
 	let catalog = new Catalog();
@@ -13,10 +14,8 @@ createConnection().then(async connection => {
 	catalog.author = "Steve Muench";
 	catalog.isPublished = true;
 
-	let catalogRepository = connection.getRepository(Catalog);
-
-	await catalogRepository.save(catalog);
-	console.log("Saved a new user with id: " + catalog.id);
+	// await catalogRepository.save(catalog);
+	// console.log("Saved a new user with id: " + catalog.id);
 
 	let catalog2 = new Catalog();
 	catalog2.journal = "Oracle Magazine";
@@ -26,13 +25,36 @@ createConnection().then(async connection => {
 	catalog2.author = "David A. Kelly";
 	catalog2.isPublished = true;
 
-	await catalogRepository.save(catalog2);
-	console.log("Saved a new user with id: " + catalog2.id);
+	// await catalogRepository.save(catalog2);
+	// console.log("Saved a new user with id: " + catalog2.id);
 
+	// select
     console.log("Loading users from the database...");
 	let [all_catalog, CatalogsCount] = await catalogRepository.findAndCount();
 	console.log("Loaded users: ", CatalogsCount);
+	let allCatalogs = await catalogRepository.find({ select: ["title", "author"] });
+	console.log("All Catalogs from the db: ", JSON.stringify(allCatalogs) + '\n');
+	let firstCatalog = await catalogRepository.findOne(1);
+	console.log('First Catalog from the db: ', JSON.stringify(firstCatalog) + '\n');
+	let specificTitleCatalog = await catalogRepository.findOne({ title: "Engineering as a Service" });
+	console.log("'Engineering as a Service' Catalog from the db: ", JSON.stringify(specificTitleCatalog) + '\n');
+	let allSpecificEditionCatalogs = await catalogRepository.find({ edition: "November December 2013" });
+	console.log('All November December 2013 Catalogs: ', JSON.stringify(allSpecificEditionCatalogs) + '\n');
+	let allPublishedCatalogs = await catalogRepository.find({ isPublished: true });
+	console.log('All published Catalogs: ', JSON.stringify(allPublishedCatalogs) + '\n');
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+	// update
+	let catalogToUpdate = await catalogRepository.findOne(1);
+	console.log('Updated Catalog from the db: ', JSON.stringify(catalogToUpdate) + '\n');
+	catalogToUpdate.title = "Beginning with Oracle PDF";
+	await catalogRepository.save(catalogToUpdate);
+	let updatedCatalog = await catalogRepository.findOne(1);
+	console.log('Updated Catalog from the db: ', JSON.stringify(updatedCatalog) + '\n');
+
+	// delete
+	let catalogToRemove = await catalogRepository.findOne(1);
+	await catalogRepository.remove(catalogToRemove);
+	let reFirstCatalog = await catalogRepository.findOne(1);
+	console.log("First Catalog from the db: ", JSON.stringify(reFirstCatalog));
 
 }).catch(error => console.log(error));
