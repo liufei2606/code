735c3fc773e7cb239e06b76523c3e0744fa27fb2
Henry <liboming88@yeah.net>
2019-06-13 19:28:30 +0800

    add channel

--- PHP/controllers/SwooleController.php
+++ PHP/controllers/SwooleController.php
@@ -1,11 +1,13 @@
 <?php
 
+use Swoole\Coroutine\Channel;
+
 /**
- * 
+ *
  */
 class SwooleController
 {
-	
+
 	public function home($request){
 		$get = isset($request->get) ? $request->get : [];
 		$result = '<h1> 你好</h1>　<br>，ＧＥＴ参数：'. json_encode($get);
@@ -15,9 +17,41 @@ class SwooleController
 
 	public function login($request){
 		$get = isset($request->get) ? $request->get : [];
-		$result = '登录成功'.;
+		$result = '登录成功';
 
 		return $result;
-	}
+    }
 
+	public function channel() {
+		go(function () {
+			//创建十个信箱通道
+			$mailBoxes = [];
+			for ($i = 1;$i <= 10;$i++) {
+				$mailBoxes[$i] = new Channel(16);
+			}
+			//模拟master 邮局调度,随机像一个信箱投递消息
+			go(function () use ($mailBoxes) {
+				while (1) {
+					\co::sleep(2);
+					$key = rand(1, 10);
+					($mailBoxes[$key])->push(time());
+				}
+			});
+			//模拟actor 实体消费
+			for ($i = 1;$i <= 10; $i++) {
+				go(function () use ($mailBoxes,$i) {
+					while (1) {
+						$msg = ($mailBoxes[$i])->pop();
+						echo "Actor {$i} recv msg : {$msg} \n";
+					}
+				});
+			}
+		});
+
+	}
 }
+
+$instance = new SwooleController();
+$instance->channel();
+
+
