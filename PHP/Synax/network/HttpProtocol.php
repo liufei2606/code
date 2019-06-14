4939210dc3a3de34223636b054ca73af6db1e43a
Henry <liboming88@yeah.net>
2019-06-14 20:51:33 +0800

    add php tcp sample

---
+++ PHP/Synax/network/HttpProtocol.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * php实现简单的http协议
+ */
+class HttpProtocol
+{
+    /**
+     * 原始请求字符串
+     *
+     * @var string
+     */
+    public  $originRequestContentString = '';
+
+    /**
+     * 原始请求字符串拆得的列表
+     *
+     * @var array
+     */
+    private $originRequestContentList = [];
+
+    /**
+     * 原始请求字符串拆得的键值对
+     *
+     * @var array
+     */
+    private $originRequestContentMap = [];
+
+    /**
+     * 定义响应头信息
+     *
+     * @var array
+     */
+    private $responseHead = [
+        'http'         => 'HTTP/1.1 200 OK',
+        'content-type' => 'Content-Type: text/html',
+        'server'       => 'Server: php/0.0.1',
+    ];
+
+    /**
+     * 定义响应体信息
+     *
+     * @var string
+     */
+    private $responseBody = '';
+
+    /**
+     * 响应内容
+     *
+     * @var string
+     */
+    public  $responseData = '';
+
+    /**
+     * 解析请求信息
+     *
+     * @param string $content
+     * @return void
+     */
+    public function request($content = '')
+    {
+        if (empty($content)) {
+            // exception
+
+        }
+        $this->originRequestContentList = explode("\r\n", $this->originRequestContentString);
+        if (empty($this->originRequestContentList)) {
+            // exception
+
+        }
+        foreach ($this->originRequestContentList as $k => $v) {
+            if ($v === '') {
+                // 过滤空
+                continue;
+            }
+            if ($k === 0) {
+                // 解析http method/request_uri/version
+                list($http_method, $http_request_uri, $http_version) = explode(' ', $v);
+                $this->originRequestContentMap['Method'] = $http_method;
+                $this->originRequestContentMap['Request-Uri'] = $http_request_uri;
+                $this->originRequestContentMap['Version'] = $http_version;
+                continue;
+            }
+            list($key, $val) = explode(': ', $v);
+            $this->originRequestContentMap[$key] = $val;
+        }
+    }
+
+    /**
+     * 组装响应内容
+     *
+     * @param [type] $responseBody
+     * @return void
+     */
+    public function response($responseBody)
+    {
+        $count = count($this->responseHead);
+        $finalHead = '';
+        foreach ($this->responseHead as $v) {
+            $finalHead .= $v . "\r\n";
+        }
+        $this->responseData = $finalHead . "\r\n" . $responseBody;
+    }
+}
---
+++ PHP/Synax/network/tcp.php
@@ -0,0 +1,32 @@
+<?php
+
+require('HttpProtocol.php');
+
+$server = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_bind($server, '127.0.0.1', '8889');
+socket_listen($server);
+
+while (true) {
+    // accept
+    $client = socket_accept($server);
+    if (! $client) {
+        continue;
+    }
+	$request = socket_read($client, 1024);
+
+	// 查看接收到的内容
+    // var_dump($request);
+
+	 /**
+     * HTTP
+     */
+    $http = new HttpProtocol;
+    $http->originRequestContentString = $request;
+    $http->request($request);
+    $http->response("Hello World");
+    $http->response($request);
+    socket_write($client, $http->responseData);
+
+    socket_close($client);
+    echo socket_strerror(socket_last_error($server)) . "\n";
+}
