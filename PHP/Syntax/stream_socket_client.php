de31f3cc9e130791febef497519ab4b920c739f0
Henry <liboming88@yeah.net>
2019-07-08 18:15:36 +0800

    update multipy  stream socket

--- PHP/Syntax/stream_socket_client.php
+++ PHP/Syntax/stream_socket_client.php
@@ -1,43 +1,53 @@
 <?php
 
-$host = "127.0.0.1";
-$port = 8009;
+        $host = "127.0.0.1";
+        $port = 8009;
+for ($index = 0; $index < 10; ++ $index) {
+    $pid = pcntl_fork();
+    if ($pid < 0) {
+        fwrite(STDERR, "fail to fork!\n");
+        exit;
+    }
 
-$socket = @stream_socket_client("tcp://{$host}:{$port}", $errno, $errMsg);
-if ($socket === false) {
-    throw new \RuntimeException("unable to create socket: " . $errMsg);
+    if ($pid === 0) {
+        $socket = @stream_socket_client("tcp://{$host}:{$port}", $errno, $errMsg);
+        if ($socket === false) {
+            throw new \RuntimeException("unable to create socket: " . $errMsg);
+        }
+        fwrite(STDOUT, "success connect to server: [{$host}:{$port}]...\n");
+
+        foreach (range(1, 10) as $i) {
+            if ($i % 5 === 0) {
+                $method = "broadcast";
+            } else {
+                $method = "echo";
+            }
+            $args = [sprintf("The %dth greeting", $i)];
+            $message = json_encode([
+                "method" => $method,
+                "args" => $args,
+            ]);
+            fwrite(STDOUT, "\nsend to server: $message\n");
+            $len = @fwrite($socket, $message);
+            if ($len === 0) {
+                fwrite(STDOUT, "socket closed\n");
+                break;
+            }
+
+            $msg = @fread($socket, 4096);
+            if ($msg) {
+                fwrite(STDOUT, "receive server: $msg\n");
+            } elseif (feof($socket)) {
+                fwrite(STDOUT, "socket closed\n");
+                break;
+            }
+
+            sleep(2);
+        }
+
+        fwrite(STDOUT, "close connnection...\n");
+        fclose($socket);
+        exit;
+    }
 }
-fwrite(STDOUT, "success connect to server: [{$host}:{$port}]...\n");
-
-foreach (range(1, 10) as $i) {
-    if ($i % 5 === 0) {
-        $method = "broadcast";
-    } else {
-        $method = "echo";
-    }
-    $args = [sprintf("The %dth greeting", $i)];
-    $message = json_encode([
-        "method" => $method,
-        "args" => $args,
-    ]);
-    fwrite(STDOUT, "\nsend to server: $message\n");
-    $len = @fwrite($socket, $message);
-    if ($len === 0) {
-        fwrite(STDOUT, "socket closed\n");
-        break;
-    }
-
-    $msg = @fread($socket, 4096);
-    if ($msg) {
-        fwrite(STDOUT, "receive server: $msg\n");
-    }
-    elseif (feof($socket)) {
-       fwrite(STDOUT, "socket closed\n");
-       break;
-    }
-
-    sleep(2);
-}
-
-fwrite(STDOUT, "close connnection...\n");
-fclose($socket);
+// 父进程先退出，不会出现僵尸进程，忽略孤儿进程的处理
--- PHP/Syntax/stream_socket_server.php
+++ PHP/Syntax/stream_socket_server.php
@@ -8,25 +8,91 @@ if ($socket === false) {
 fwrite(STDOUT, "socket server listen on port: {$port}" . PHP_EOL);
 stream_set_blocking($socket, false);
 
+// while (true) {
+//     $client = @stream_socket_accept($socket);
+//     if ($client == false) {
+//         continue;
+//     }
+
+//     fwrite(STDOUT, "client:" . (int)$client . " connnected.\n");
+//     @fwrite($client, "Welcome aboard!\n");
+
+//     while (true) {
+//         $msg = @fread($client, 4096);
+//         if ($msg) {
+//             fwrite(STDOUT, "\nreceive client: $msg\n");
+//             // echo
+//             @fwrite($client, $msg);
+//         } elseif (feof($client)) {
+//             fwrite(STDOUT, "client:" . (int)$client . " disconnnect!\n");
+//             fclose($client);
+//             break;
+//         }
+//     }
+// }
+
+$clients = [];
+$changed = [];
 while (true) {
-    $client = @stream_socket_accept($socket);
-    if ($client == false) {
-        continue;
+    checkMessage();
+    fwrite(STDOUT, "\nnew read message\n");
+    accept();
+    handleMessage();
+}
+
+function checkMessage() {
+    global $socket, $changed, $clients;
+    $changed = array_merge([$socket], $clients);
+    $write = null;
+    $except = null;
+    stream_select($changed, $write, $except, null);
+}
+
+function accept() {
+    global $socket, $changed, $clients;
+    if (!in_array($socket, $changed)) {
+        return;
     }
 
-    fwrite(STDOUT, "client:" . (int)$client . " connnected.\n");
-    @fwrite($client, "Welcome aboard!\n");
+    while ($client = @stream_socket_accept($socket, 0)) {
+        $clients[] = $client;
 
-    while (true) {
-        $msg = @fread($client, 4096);
-        if ($msg) {
-            fwrite(STDOUT, "\nreceive client: $msg\n");
-            // echo
-            @fwrite($client, $msg);
-        } elseif (feof($client)) {
-            fwrite(STDOUT, "client:" . (int)$client . " disconnnect!\n");
-            fclose($client);
-            break;
+        fwrite(STDOUT, "client:" . (int)$client . " connnected.\n");
+        fwrite($client, "welcome aboard!");
+        stream_set_blocking($client, false);
+
+        $key = array_search($client, $changed);
+        unset($changed[$key]);
+    }
+}
+
+function handleMessage() {
+    global $changed, $clients;
+    foreach ($changed as $key => $client) {
+        while (true) {
+            $msg = @fread($client, 4096);
+            if ($msg) {
+                fwrite(STDOUT, "receive client " . (int)$client . " message: $msg\n");
+                $json = json_decode($msg, true);
+                if ($json) {
+                    $method = $json["method"];
+                    if ($method === 'echo') {
+                        @fwrite($client, $msg);
+                    } else {
+                        foreach ($clients as $cl) {
+                            @fwrite($cl, "message from " . (int)$client . ": $msg");
+                        }
+                    }
+                }
+            } else {
+                if (feof($client)) {
+                    fwrite(STDOUT, "\nclient " . (int)$client . " closed.\n");
+                    fclose($client);
+                    $key = array_search($client, $clients);
+                    unset($clients[$key]);
+                }
+                break;
+            }
         }
     }
 }
