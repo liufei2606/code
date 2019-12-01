4ed4d10a48f5230d004cbeb50362806322819436
Henry <liboming88@yeah.net>
2019-12-01 11:29:32 +0800

    add service check

--- MutilMedia/js/client.js
+++ MutilMedia/js/client.js
@@ -14,6 +14,24 @@ function handleLocalMediaStreamError(error) {
     console.log('navigator.getUserMedia error: ', error);
 }
 
+if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    console.log("enumerateDevices() not supported.");
+    // return;
+}
+
+
+// 枚举 cameras and microphones.
+navigator.mediaDevices.enumerateDevices()
+    .then(function(deviceInfos) {
+        //打印出每一个设备的信息
+        deviceInfos.forEach(function(deviceInfo) {
+            console.log(deviceInfo.kind + ": " + deviceInfo.label + " id = " + deviceInfo.deviceId);
+        });
+    })
+    .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+    });
+
 navigator.mediaDevices.getUserMedia(mediaStreamContrains).then(
     getLocalMediaStream
 ).catch(
