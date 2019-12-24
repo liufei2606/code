1086b8ab19f23123a1c375efa28028fccb95d037
Henry <liboming88@yeah.net>
2019-12-24 15:26:53 +0800

    add swoole framework

---
+++ PHP/Swoole/application/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'i am family by route';
+    }
+
+    public function tong()
+    {
+        return 'i am tong ge';
+    }
+}
---
+++ PHP/Swoole/framework/Family/Core/Route.php
@@ -0,0 +1,24 @@
+<?php
+namespace Family\Core;
+
+class Route
+{
+    public static function dispatch($path)
+    {
+
+        //默认访问 controller/index.php 的 index方法
+        if (empty($path) || '/' == $path || "/favicon.ico" == $path) {
+            $controller = 'Index';
+            $method = 'Index';
+        } else {
+            $maps = explode('/', $path);
+            $controller = $maps[1];
+            $method = $maps[2];
+        }
+
+        $controllerClass = "controller\\{$controller}";
+        $class = new $controllerClass;
+
+        return $class->$method();
+    }
+}
