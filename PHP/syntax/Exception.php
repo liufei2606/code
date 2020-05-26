62cc314547794d31d23f151823e5de9aee6c4c9d
henry <liboming88@yahoo.com>
2020-05-26 22:17:01 +0800

    tidy

--- PHP/syntax/Exception.php
+++ PHP/syntax/Exception.php
@@ -1,39 +1,23 @@
 <?php
 
-$instance = new \Exception('exception example', 600);
-
-echo $instance->getMessage(). '<br>';
-echo $instance->getCode() . '<br>';
-
-/**
- * 自定义一个异常处理类
- */
 class MyException extends Exception
 {
-    // 重定义构造器使 message 变为必须被指定的属性
     public function __construct($message, $code = 0, Exception $previous = null)
     {
-        // 自定义的代码
-
-        // 确保所有变量都被正确赋值
         parent::__construct($message, $code, $previous);
     }
 
-    // 自定义字符串输出的样式
     public function __toString()
     {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        return __CLASS__.": [{$this->code}]: {$this->message}".PHP_EOL;
     }
 
     public function customFunction()
     {
-        echo "A custom function for this type of exception\n";
+        echo "A custom function for this type of exception".PHP_EOL;
     }
 }
 
-/**
- * 创建一个用于测试异常处理机制的类
- */
 class TestException
 {
     public $var;
@@ -46,36 +30,27 @@ class TestException
     {
         switch ($avalue) {
             case self::THROW_CUSTOM:
-                // 抛出自定义异常
                 throw new MyException('1 is an invalid parameter', 5);
                 break;
-
             case self::THROW_DEFAULT:
-                // 抛出默认的异常
                 throw new Exception('2 is not allowed as a parameter', 6);
                 break;
-
             default:
-                // 没有异常的情况下，创建一个对象
                 $this->var = $avalue;
                 break;
         }
     }
 }
 
-
-// 例子 1
 try {
     $o = new TestException(TestException::THROW_CUSTOM);
-} catch (MyException $e) {      // 捕获异常
+} catch (MyException $e) {
     echo "Caught my exception\n", $e;
     $e->customFunction();
-} catch (Exception $e) {        // 被忽略
+} catch (Exception $e) {
     echo "Caught Default Exception\n", $e;
 }
-
-// Continue execution
-var_dump($o); // Null
+var_dump($o);
 echo "\n\n";
 
 // 例子 2
@@ -116,3 +91,145 @@ try {
 // 执行后续代码
 var_dump($o); // TestException
 echo "\n\n";
+
+
+header("Content-type:text/html;charset=utf-8");
+
+# 系统自带
+try {
+    //业务处理 错误时抛出异常。
+    $age = 130;
+    if ($age > 120) {
+        throw new Exception('年龄不能大于120岁。', 1001);
+    }
+} catch (Exception $e) {
+    $err = [
+        'code' => $e->getCode(),
+        'msg' => $e->getMessage(),
+        'file' => $e->getFile(),
+        'line' => $e->getLine()
+    ];
+    echo json_encode($err).PHP_EOL;
+}
+
+# 自定义
+class proException extends Exception
+{
+    public function getErrorInfo($type = 2)
+    {
+        $err = [
+            'code' => $this->getCode(),
+            'msg' => $this->getMessage(),
+            'file' => $this->getFile(),
+            'line' => $this->getLine()
+        ];
+        if ($type == 1) {
+            return json_encode($err);
+        }
+        return $err;
+    }
+}
+
+try {
+    //业务处理 错误时抛出异常。
+    $age = 130;
+    if ($age > 120) {
+        throw new proException('年龄不能大于120岁。', 1001);
+    }
+} catch (proException $e) {
+    $info = $e->getErrorInfo();
+    var_dump($info);
+    echo PHP_EOL;
+}
+
+try {
+    if ($_GET['age'] > 100) {
+        throw new multyException('自定义的异常处理', 1002);
+    } else {
+        throw new Exception('系统的异常处理', 1002);
+    }
+} catch (proException $e) {
+    $info = $e->getErrorInfo();
+    var_dump($info);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
+error_reporting(0);
+//设置错误处理器
+set_error_handler('errorHandler');
+//在脚本结束时运行的函数
+register_shutdown_function('fatalErrorHandler');
+
+/**
+ * 错误处理
+ *
+ * @param  int     $err_no    错误代码
+ * @param  string  $err_msg   错误信息
+ * @param  string  $err_file  错误文件
+ * @param  int     $err_line  错误行号
+ *
+ * @return string
+ */
+function errorHandler($err_no = 0, $err_msg = '', $err_file = '', $err_line = 0)
+{
+    $log = [
+        '['.date('Y-m-d h-i-s').']',
+        '|',
+        $err_no,
+        '|',
+        $err_msg,
+        '|',
+        $err_file,
+        '|',
+        $err_line
+    ];
+    $log_path = './debug.log';
+    error_log(implode(' ', $log)."\r\n", 3, $log_path);
+    //echo implode(' ',$log)."<br>";
+}
+
+/**
+ * 捕捉致命错误
+ *
+ * @return string
+ */
+function fatalErrorHandler()
+{
+    $e = error_get_last();
+    switch ($e['type']) {
+        case 1:
+            errorHandler($e['type'], $e['message'], $e['file'], $e['line']);
+            break;
+    }
+}
+
+class DemoClass_1
+{
+    public function index()
+    {
+        echo $undefinedVarible;
+    }
+}
+
+$demo_1 = new DemoClass_1();
+$demo_1->index();
+$demo_2 = new DemoClass_2();
+$demo_2->index();
+
+function getItemFromBook($book, $key)
+{
+    if (empty($book) || !key_exists($key, $book)) {
+        throw new InvalidArgumentException("数组为空或者对应索引不存在！");
+    }
+    return $book[$key];
+}
+
+try {
+    $val = getItemFromBook($book, 'desc');
+} catch (InvalidArgumentException $exception) {
+    echo $exception->getMessage();
+    exit();
+}
+var_dump($val);
---
+++ PHP/syntax/error.php
@@ -0,0 +1,58 @@
+<?php
+
+ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+error_reporting(E_ALL ^ E_WARNING);
+
+/**
+ * 自定义错误处理器
+ *
+ * @param $errno    int 错误级别
+ * @param $errstr   string 错误信息
+ * @param $errfile  string 错误文件
+ * @param $errline  int   错误行号
+ */
+function myErrorHandler($errno, $errstr, $errfile, $errline)
+{
+    // 该级别错误不报告的话退出
+    if (!(error_reporting() & $errno)) {
+        return;
+    }
+    $logDir = __DIR__."/..".DIRECTORY_SEPARATOR.'logs';
+    if (!file_exists($logDir)) {
+        mkdir($logDir);
+    }
+    $logFile = $logDir.DIRECTORY_SEPARATOR.'err.log';
+
+//    logger("%file% %level% %message%", ["level" => "warning", "message" => "This is a message", "file" => __FILE__]);
+    switch ($errno) {
+        case E_ERROR:
+            error_log("[致命错误]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr", 3, $logFile);
+            break;
+        case E_WARNING:
+            error_log("[警告]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr", 3, $logFile);
+            break;
+        case E_NOTICE:
+            error_log("[通知]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr", 3, $logFile);
+            break;
+        default:
+            echo "[未知错误类型]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr\n";
+            break;
+    }
+}
+
+//set_error_handler("myErrorHandler");
+$ctx = stream_context_create(array(
+        'http' => array(
+            'timeout' => 1
+        )
+    )
+);
+try {
+    $content = file_get_contents('https://bluebird89.com/error', 0., $ctx);
+
+} catch (Error $e) {
+    var_dump($e);
+}
+
+
