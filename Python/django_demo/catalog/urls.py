e1709917d0b740910ab54cbaf796a26cbb248183
henry <liboming88@yahoo.com>
2020-05-28 22:21:59 +0800

    add:front main page

--- Python/django_demo/catalog/urls.py
+++ Python/django_demo/catalog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
 from catalog import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('books', views.books, name='books'),
+    path('authors', views.books, name='books'),
+    path('book/<id>', views.books, name='books'),
+    path('author/<id>', views.books, name='books'),
 
 ]
