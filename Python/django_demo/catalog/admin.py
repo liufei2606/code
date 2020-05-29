868fb9ea1fe1a3efcb77a849f3b385b39dd6a368
henry <liboming88@yahoo.com>
2020-05-29 10:59:12 +0800

    add:user authorsize and permission

--- Python/django_demo/catalog/admin.py
+++ Python/django_demo/catalog/admin.py
@@ -6,10 +6,13 @@ from .models import Author, Genre, Book, BookInstance
 # admin.site.register(Book)
 # admin.site.register(Author)
 admin.site.register(Genre)
+
+
 # admin.site.register(BookInstance)
 
 class BookInline(admin.TabularInline):
     model = Book
+
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
@@ -17,29 +20,33 @@ class AuthorAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BookInline]
 
+
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
+
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
 
+
 # Register the Admin classes for BookInstance using the decorator
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back', 'id')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
         (None, {
-            'fields': ('book','imprint', 'id')
+            'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
--- Python/django_demo/catalog/models.py
+++ Python/django_demo/catalog/models.py
@@ -1,6 +1,5 @@
+from django.contrib.auth.models import User
 from django.db import models
-
-# Create your models here.
 
 
 class MyModelName(models.Model):
@@ -29,6 +28,7 @@ class MyModelName(models.Model):
         """
         return self.field_name
 
+
 class Genre(models.Model):
     """
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
@@ -42,7 +42,9 @@ class Genre(models.Model):
         """
         return self.name
 
+
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+
 
 class Book(models.Model):
     """
@@ -76,11 +78,13 @@ class Book(models.Model):
         """
         Creates a string for the Genre. This is required to display genre in Admin.
         """
-        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
     display_genre.short_description = 'Genre'
 
-import uuid  # Required for unique book instances
 
+import uuid  # Required for unique book instances
+from datetime import date
 
 class BookInstance(models.Model):
     """
@@ -91,6 +95,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -103,12 +108,19 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """
         String for representing the Model object
         """
         return '%s (%s)' % (self.id, self.book.title)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 class Author(models.Model):
     """
