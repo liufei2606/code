7f423966cf6d7335a826e97edae905adf203b9f4
henry <liboming88@yahoo.com>
2020-06-06 11:07:49 +0800

    add:chitchat session for authenticate

--- Go/src/chitchat/handlers/index.go
+++ Go/src/chitchat/handlers/index.go
@@ -2,16 +2,18 @@ package handlers
 
 import (
 	"chitchat/models"
-	"html/template"
 	"net/http"
 )
 
 // 论坛首页路由处理器方法
 func Index(w http.ResponseWriter, r *http.Request) {
-	files := []string{"views/layout.html", "views/navbar.html", "views/index.html"}
-	templates := template.Must(template.ParseFiles(files...))
 	threads, err := models.Threads()
 	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
+		_, err := session(w, r)
+		if err != nil {
+			generateHTML(w, threads, "layout", "navbar", "index")
+		} else {
+			generateHTML(w, threads, "layout", "auth.navbar", "index")
+		}
 	}
 }
