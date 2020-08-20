34dcbb3af9f75a9342a83537b9da49a795d05fcf
henry <liboming88@yahoo.com>
2020-08-20 09:22:53 +0800

    format:vue

--- Vue/syntax/components/button_counter.js
+++ Vue/syntax/components/button_counter.js
@@ -1,8 +1,9 @@
-Vue.component('button-counter', {
+Vue.component("button-counter", {
     data: function () {
-      return {
-        count: 0
-      }
+        return {
+            count: 0,
+        };
     },
-    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-  })
+    template:
+        '<button v-on:click="count++">You clicked me {{ count }} times.</button>',
+});
