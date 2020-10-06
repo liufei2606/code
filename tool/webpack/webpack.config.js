08295c1669380519bb554d6763b8cbd3f8ada858
Henryli <liboming88@yeah.net>
2020-10-07 03:50:22 +0800

    webpack:failed add jsx loader

--- tool/webpack/webpack.config.js
+++ tool/webpack/webpack.config.js
@@ -1,9 +1,23 @@
 module.exports = {
+	mode: 'development',
 	entry: {
 		bundle1: './src/main1.js',
-		bundle2: './src/main2.js'
-	  },
-	  output: {
+		bundle2: './src/main2.js',
+		// main: './src/main.jsx'
+	},
+	output: {
 		filename: './js/[name].js'
-	  }
+	},
+	// module: {
+	// 	rules: [{
+	// 		test: /\.jsx?$/,
+	// 		exclude: /node_modules/,
+	// 		use: {
+	// 			loader: 'babel-loader',
+	// 			options: {
+	// 				presets: ['es2015', 'react']
+	// 			}
+	// 		}
+	// 	}]
+	// }
 };
