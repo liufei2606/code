4ffed141bc21a9f0e1108526f784bfc3f62f8421
Henryli <liboming88@yeah.net>
2020-10-07 07:05:19 +0800

    add:webpack config

--- tool/webpack/webpack.config.js
+++ tool/webpack/webpack.config.js
@@ -1,23 +1,36 @@
-module.exports = {
-	mode: 'development',
-	entry: {
-		bundle1: './src/main1.js',
-		bundle2: './src/main2.js',
-		// main: './src/main.jsx'
-	},
-	output: {
-		filename: './js/[name].js'
-	},
-	// module: {
-	// 	rules: [{
-	// 		test: /\.jsx?$/,
-	// 		exclude: /node_modules/,
-	// 		use: {
-	// 			loader: 'babel-loader',
-	// 			options: {
-	// 				presets: ['es2015', 'react']
-	// 			}
-	// 		}
-	// 	}]
-	// }
-};
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// 读取同一目录下的 base config
+const config = require('./webpack.base.config');
+
+config.module.rules.push({
+	test: /\.less$/,
+	use: ExtractTextPlugin.extract({
+		use: [
+			'css-loader',
+			'less-loader'
+		],
+		fallback: 'style-loader'
+	}),
+	exclude: /node_modules/
+});
+
+config.plugins.push(
+	// 官方文档推荐使用下面的插件确保 NODE_ENV
+	new webpack.DefinePlugin({
+		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+	}),
+	// 启动 minify
+	new webpack.LoaderOptionsPlugin({
+		minimize: true
+	}),
+	// 抽取 CSS 文件
+	new ExtractTextPlugin({
+		filename: '[name].css',
+		allChunks: true,
+		ignoreOrder: true
+	})
+);
+
+module.exports = config;
