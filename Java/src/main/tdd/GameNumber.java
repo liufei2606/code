39f8720a6208a6e18c240ff490502638a61afb23
Henryli <liboming88@yeah.net>
2020-10-12 16:12:44 +0800

    add:java fizzbuzz

--- Java/src/main/tdd/GameNumber.java
+++ Java/src/main/tdd/GameNumber.java
@@ -1,4 +1,43 @@
 package tdd;
 
 public class GameNumber {
+	private int i;
+
+	public GameNumber(int i) {
+		this.i = i;
+	}
+
+	public String toString1() {
+		if (this.i % 3 == 0) {
+			return "Fizz";
+		} else if (this.i % 5 == 0) {
+			return "Buzz";
+		} else if (this.i % 3 == 0 && this.i % 5 == 0) {
+			return "FizzBuzz";
+		}
+
+		return String.valueOf(this.i);
+	}
+
+	public String toString() {
+		String rs = "";
+
+		if (this.i % 3 == 0) {
+			rs += "Fizz";
+		}
+		if (this.i % 5 == 0) {
+			rs += "Buzz";
+		}
+
+		if (rs.equals("")) {
+			rs = String.valueOf(this.i);
+		}
+
+		return rs;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i<100; i++){
+		}
+	}
 }
--- Java/src/test/tdd/GameNumberTest.java
+++ Java/src/test/tdd/GameNumberTest.java
@@ -2,10 +2,32 @@ package tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameNumberTest {
 
+
 	@Test
-	void should_create_game_number_from_raw_number(){
-	 GameNumber gameNumber =  new GameNumber();
+	void should_create_game_number_from_raw_number() {
+		GameNumber gameNumber = new GameNumber(1);
+		assertEquals("1", gameNumber.toString());
+	}
+
+	@Test
+	void should_create_fizz_from_divied_by_3() {
+		GameNumber gameNumber = new GameNumber(9);
+		assertEquals("Fizz", gameNumber.toString());
+	}
+
+	@Test
+	void should_create_buzz_from_divied_by_5() {
+		GameNumber gameNumber = new GameNumber(10);
+		assertEquals("Buzz", gameNumber.toString());
+	}
+
+	@Test
+	void should_create_fizzbuzz_from_divied_by_3_and_5() {
+		GameNumber gameNumber = new GameNumber(30);
+		assertEquals("FizzBuzz", gameNumber.toString());
 	}
 }
