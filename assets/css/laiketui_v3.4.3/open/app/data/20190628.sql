8a45d829f1c8655c639c08ae15d308588769c009
Henryli <liboming88@yeah.net>
2020-10-26 12:36:06 +0800

    tidy:assets

--- assets/css/laiketui_v3.4.3/open/app/data/20190628.sql
+++ assets/css/laiketui_v3.4.3/open/app/data/20190628.sql
@@ -1,3 +1,3 @@
-
+git
 
 ALTER TABLE `lkt_return_goods` ADD `address` VARCHAR(225) NOT NULL COMMENT '买家地址' AFTER `user_id`;
--- assets/css/laiketui_v3.4.3/open/app/install/webapp/modules/Default/actions/UnavailableAction.class.php
+++
@@ -1,1 +0,0 @@
-<?phpclass UnavailableAction extends Action{    public function execute ()    {    }    public function getDefaultView ()    {        return View::SUCCESS;    }    public function getRequestMethods ()    {        return Request::NONE;    }}?>
